bc801ea3f37f6e47f28f3a1a596a7c72438da794
sultanasafia <147336363+sultanasafia@users.noreply.github.com>
2024-08-16 17:28:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+// CH - 1==============
+
+// Q1
+
+// alert("Hello")
+
+// Q2
+
+// alert("Erorr! Please Enter a Valid Password")
+
+// Q3
+
+// alert("Welcome to js Land \n Happy Coding")
+
+//Q4
+
+// alert("welcome to js land")
+// alert("Happy coding")
+
+//Q5
+
+// alert("Hello... I can run js through my web browser's console")
+
+//Q6
+
+// alert("welcome my website")
+
+// CH-1 COMPLETED===============
+
+// CH-2 START===================
+// Q1
+//  let userName;
+// Q2
+// let myName = "Safia Sultana"
+// document.write(myName)
+//Q3
+// let message = "Hello World"
+// alert(message)
+
+// Q4
+// let myName = "Safia";
+// let age = 20;
+// let message = "Certified Mobile Developement Application"
+
+// alert(myName)
+// alert(age)
+// alert(message)
+// Q5
+
+// let message = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(message)
+// Q6
+// let email = "safiabano736@gmail.com"
+// alert("My email address is "+ email)
+
+// Q7
+// let message = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the book "+message)
+
+// Q8
+
+// let message = "Yah! I can write HTML content through javascript";
+// document.write(message)
+
+// Q9
+
+// let pic = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(pic)
+
+
+// ch-2 complete complete=============
+
+
+
+// ch-3 start =======================
+
+
+// Q-1
+
+// let age = 20;
+// alert("I am "+age +" Years Old")
+
+// Q2
+
+// let n = 14;
+// alert("You have Visited this site 14 times")
+
+// Q3
+// let birthYear = 1991;
+// document.write("My birth year is "+ birthYear)
+// document.write("<br>")
+// document.write("Data type of my decleared variable is number")
+
+// Q4
+// let visitorName = prompt("Enter your name")
+// let product = prompt("ordered 5 T-shirt(s)")
+// let cloStore = "XYZ Clothing Store"
+// document.write(visitorName+" "+product+" " + cloStore)
+
+
+// CHAPTER 3 COMPLETED=============
